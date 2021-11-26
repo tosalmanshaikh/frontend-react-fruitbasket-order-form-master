f55aa890c2b2192fb0714e4416b08219ddcabd11
tosalmanshaikh <tosalmanshaikh@gmail.com>
2021-11-26 16:56:16 +0100

    submit2

--- src/components/Article.js
+++ src/components/Article.js
@@ -34,38 +34,3 @@ export default Article;
 
 
 
-
-//a
-// import React from 'react';
-//
-//
-// function Article (props) {
-//     console.log(props)
-//
-//     return (
-//         <article className="number">
-//             <h2>
-//                 <p>{props.emoji}</p>
-//                 {props.text}
-//             </h2>
-//             <button
-//                 type={props.type}
-//                 name='Aftellen'
-//                 onClick={() => props.setFruitQty(props.fruitQty-1)}
-//                 disabled={props.disabled}
-//             >-
-//             </button>
-//             <p>{props.fruitQty}</p>
-//
-//             <button
-//                 type={props.type}
-//                 name='Optellen'
-//                 onClick={() => props.setFruitQty(props.fruitQty+1)}
-//             >+
-//             </button>
-//         </article>
-//     )
-// }
-//
-// export default Article;
-
